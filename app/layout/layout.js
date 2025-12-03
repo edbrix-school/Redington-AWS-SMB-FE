1684e089b3d98d163728e6a760e54c2b5ad90128
Poonam <poonamk@hexalytics.com>
2025-12-03 17:35:04 +0530

    top-html

---
+++ app/layout/layout.js
@@ -0,0 +1,29 @@
+import Footer from "./footer";
+import Top from "./top";
+import Head from "next/head";
+
+
+
+
+export default function Layout({ children, ...pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>{pageProps.pageTitle ? pageProps.pageTitle : "Loading..."} | Welcome to Redington-AWS-SMB</title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={pageProps.pageClass}>
+        <Top />                 
+        <div>
+          <main>
+            {children}
+          </main>
+        </div>
+        <Footer/>
+    
+      </div>
+    </>
+  );
+}
