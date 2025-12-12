08c48bd07ac5e6d29fe16e410b97f142092d639f
Poonam <poonamk@hexalytics.com>
2025-12-12 19:15:32 +0530

    changes

--- app/layout/top.js
+++ app/layout/top.js
@@ -20,9 +20,7 @@ export default function Top() {
   const cities = [
     { name: "New York", code: "NY" },
     { name: "Rome", code: "RM" },
-    { name: "London", code: "LDN" },
-    { name: "Istanbul", code: "IST" },
-    { name: "Paris", code: "PRS" },
+   
   ];
 
   return (
