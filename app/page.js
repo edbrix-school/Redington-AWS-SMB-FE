8b78e60b03b27d45e9383ba3e192ad415b33b06b
Sujit Yadav <86299830+sujityadav89@users.noreply.github.com>
2025-12-12 13:51:30 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -5,7 +5,11 @@ import Layout from "./layout";
 export default function Home() {
   return (
     <Layout>
+<<<<<<< Updated upstream
       <div className="flex min-h-screen items-center justify-center bg-zinc-50  ">
+=======
+      <div className="flex min-h-screen items-center justify-center bg-zinc-50">
+>>>>>>> Stashed changes
         <Landing />
       </div>
     </Layout>
