a84663233bf8b72c2d5cade2b8bb24dfb36b3e08
Abhi <apatole@hexalytics.com>
2025-12-16 17:33:05 +0530

    banner set

--- app/announcements/page.js
+++ app/announcements/page.js
@@ -10,7 +10,7 @@ export default function Announcements() {
         <div className="min-h-screen bg-gray-50 font-sans flex flex-col">
             <NavBar />
             <Hero />
-            <main className="flex-grow -mt-20 relative z-20 px-4 md:px-8 pb-10"> {/* Negative margin to overlap Hero */}
+            <main className="flex-grow -mt-20 relative z-20 pb-10"> {/* Negative margin to overlap Hero */}
                 <FilesTable />
             </main>
         </div>
--- app/user-management/page.js
+++ app/user-management/page.js
@@ -11,7 +11,7 @@ export default function page() {
     <div className="min-h-screen bg-gray-50 font-sans flex flex-col">
       <NavBar />
       <UserManagementHero />
-      <main className="flex-grow -mt-20 relative z-20 px-4 md:px-8 pb-10"> {/* Negative margin to overlap Hero */}
+      <main className="flex-grow -mt-20 relative z-20 pb-10"> {/* Negative margin to overlap Hero */}
         <Tabel />
       </main>
     </div>
